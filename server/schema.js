46259e98c3dbf39870b7d65597d45e8544925052
Fred Wang <namelos@outlook.com>
2016-12-12 00:12:08 +0800

    add todo related graphql server api

--- server/schema.js
+++ server/schema.js
@@ -1,14 +1,42 @@
 const { makeExecutableSchema } = require('graphql-tools')
 
 const Schema = `
+type Todo {
+  id: Int!
+  text: String
+}
+
 type Query {
+  todos: [Todo]
+  todo(id: Int!): Todo
   hello(name: String!): String
+}
+
+type Mutation {
+  addTodo(text: String!): Todo
 }
 `
 
+const generateId = (() => {
+  let id = 0
+  return () => id++
+})()
+
+const todos = []
+
+
 const Resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello, ${name}`
+    todos: () => todos,
+    todo: (_, {id}) => todos.find(todo => todo.id == id),
+    hello: (_, {name}) => `Hello, ${name}`
+  },
+  Mutation: {
+    addTodo: (_, {text}) => {
+      let todo = { id: generateId(), text }
+      todos.push(todo)
+      return todo
+    }
   }
 }
 
