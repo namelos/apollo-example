8410ab0a0f86f74609549d1e6150be7dca68295c
Fred Wang <namelos@outlook.com>
2016-12-11 20:18:03 +0800

    add hello in server side graphql

---
+++ server/schema.js
@@ -0,0 +1,18 @@
+const { makeExecutableSchema } = require('graphql-tools')
+
+const Schema = `
+type Query {
+  hello: String
+}
+`
+
+const Resolvers = {
+  Query: {
+    hello: () => 'GraphQL!'
+  }
+}
+
+module.exports = makeExecutableSchema({
+  typeDefs: Schema,
+  resolvers: Resolvers
+})
